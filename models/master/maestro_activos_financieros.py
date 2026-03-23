9528e8bbfdec2b5270454a0abdc68bdb06daadbc
Aitor Fernandez <aitorfernandezcas@gmail.com>
2026-03-23 22:53:51 +0100

    Actualizado Maestro Activos Financieros

--- models/master/maestro_activos_financieros.py
+++ models/master/maestro_activos_financieros.py
@@ -5,7 +5,7 @@ from ..base import Base
 class MaestroActivosFinancieros(Base):
     __tablename__ = 'maestro_activos_financieros'
 
-    id = Column(Integer, primary_key=True)
-    isin = Column(String(255), nullable=True)
+    id = Column(Integer)
+    isin = Column(String(255), primary_key=True)
     id_tipo_instrumento = Column(String(255), nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
