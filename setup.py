cf6e04c322e3a62c41ddc8792cc3a4807c1350fc
Aitor Fernandez <aitorfernandezcas@gmail.com>
2026-03-23 23:00:45 +0100

    Actualizado Maestro Activos Financieros

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="finance-tracker-models",
-    version="0.1.0",
+    version="0.1.1",
     author="Aitor Fernandez",
     author_email="your-email@example.com",
     description="Shared SQLAlchemy models for Finance Tracker projects",
