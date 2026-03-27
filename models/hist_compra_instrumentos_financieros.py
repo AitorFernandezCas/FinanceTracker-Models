caf377a53f39e017eb4eb6c1513532950bcdf4fa
Aitor Fernandez <aitorfernandezcas@gmail.com>
2026-03-27 18:53:14 +0100

    Actualizado HistCompraInstrumentosFinancieros

--- models/hist_compra_instrumentos_financieros.py
+++ models/hist_compra_instrumentos_financieros.py
@@ -6,9 +6,12 @@ class HistCompraInstrumentosFinancieros(Base):
     __tablename__ = 'hist_compra_instrumentos_financieros'
 
     fecha = Column(Date, primary_key=True)
-    id_instrumento = Column(Integer, primary_key=True)
-    flag_compra = Column(Boolean, primary_key=True)
+    ISIN = Column(Integer, primary_key=True)
     cantidad = Column(Float)
-    precio = Column(Float)
+    tipo_cambio = Column(Float)
     id_divisa = Column(String(255), nullable=True)
+    comision_autoFX = Column(Float)
+    coste_transaccion = Column(Float)
+    valor_local = Column(Float)
+    valor_EUR = Column(Float)
     created_at = Column(DateTime, default=datetime.utcnow)
